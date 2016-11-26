a2385912b67409cd4e8d33107dc3653ade6a3757
Jeferson Guardezi <jeferson@guardezi.com>
2016-11-26 21:53:13 -0200

    ajsje

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,22 +1,33 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+// import { ModalContentPage } from '../modal-receipt/modal-receipt';
+import {ModalController, NavController } from 'ionic-angular';
+import {FinishPage} from '../finish/finish';
+import { ModalContentPage } from '../modal-receipt/modal-receipt';
+
+
 
 /*
   Generated class for the Map page.
 
   See http://ionicframework.com/docs/v2/components/#navigation for more info on
   Ionic pages and navigation.
-*/
-@Component({
-  selector: 'page-map',
-  templateUrl: 'map.html'
-})
-export class MapPage {
+  */
+  @Component({
+    selector: 'page-map',
+    templateUrl: 'map.html'
+  })
+  export class MapPage {
 
-  constructor(public navCtrl: NavController) {}
+    constructor(public navCtrl: NavController, public modalCtrl:ModalController) {}
 
-  ionViewDidLoad() {
-    console.log('Hello MapPage Page');
+    ionViewDidLoad() {
+      let characterNum = {charNum: 0};
+      // let modal = this.modalCtrl.create(ModalContentPage, characterNum);
+      // modal.present();
+      console.log('Hello MapPage Page');
+    }
+
+    finishRequest(){
+      this.navCtrl.push(FinishPage);
+    }
   }
-
-}
