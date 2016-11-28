8d8800d399b9cd5cab67535068ace976c060673e
Jeferson Guardezi <jeferson@guardezi.com>
2016-11-28 00:18:32 -0200

    tour

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { TourPage } from '../pages/tour/tour';
 import { Page1 } from '../pages/page1/page1';
 import { Page2 } from '../pages/page2/page2';
 import { LoginPage } from '../pages/login/login';
--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef  } from '@angular/core';
 
 import { ModalContentPage } from '../modal-receipt/modal-receipt';
 import { NavController,ModalController, Platform, NavParams } from 'ionic-angular';
 
+declare var google;
 /*
   Generated class for the Map page.
 
@@ -14,22 +15,42 @@ import { NavController,ModalController, Platform, NavParams } from 'ionic-angula
     templateUrl: 'map.html'
   })
   export class MapPage {
+    @ViewChild('map') mapElement: ElementRef;
+    map: any;
 
     constructor(public navCtrl: NavController, public modalCtrl:ModalController) {}
 
-    ionViewDidLoad() {
-      let characterNum = {charNum: 0};
-      // let modal = this.modalCtrl.create(ModalContentPage, characterNum);
-      // modal.present();
-      console.log('Hello MapPage Page');
-      this.openModal({charNum: 0});
-    }
+    // ionViewDidLoad() {
+      //   let characterNum = {charNum: 0};
+      //   // let modal = this.modalCtrl.create(ModalContentPage, characterNum);
+      //   // modal.present();
+      //   console.log('Hello MapPage Page');
+      //   // this.openModal({charNum: 0});
+      //   this.loadMap();
+      // }
+      ionViewLoaded(){
+        this.loadMap();
+      }
 
-    finishRequest(){
-      // this.navCtrl.push(FinishPage);
+      loadMap(){
+
+        let latLng = new google.maps.LatLng(-34.9290, 138.6010);
+
+        let mapOptions = {
+          center: latLng,
+          zoom: 15,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+
+        this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+        console.log('mapaaaa');
+      }
+
+      finishRequest(){
+        // this.navCtrl.push(FinishPage);
+      }
+      openModal(characterNum){
+        let modal = this.modalCtrl.create(ModalContentPage, characterNum);
+        modal.present();
+      }
     }
-    openModal(characterNum){
-      let modal = this.modalCtrl.create(ModalContentPage, characterNum);
-      modal.present();
-    }
-  }
