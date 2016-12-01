d765b954421a97723d49de5456a978bd67dad09d
Jeferson Guardezi <jeferson@guardezi.com>
2016-12-01 19:04:28 -0200

    frase

--- src/providers/constants.ts
+++ src/providers/constants.ts
@@ -1,16 +1,18 @@
-export let API = {
-  URL:"http://api.cerveja.me/",
-  DEVICE:"device",
-  LOCATION:"location",
-  GOOGLE_GEOCODE:"https://maps.googleapis.com/maps/api/geocode/json?address=#&key=AIzaSyCviMvRgOLra4U-obeRi33K0Cur5WlGTQg",
-  GOOGLE_ADDRESS:"https://maps.googleapis.com/maps/api/geocode/json?latlng=#&key=AIzaSyCviMvRgOLra4U-obeRi33K0Cur5WlGTQg"
-};
-
 export let PHRASES =[
 "oi",
 "teste",
 "buscando gelo com o pé grande",
 "brigando com os tigres tibetanos pela sua cerveja",
 "GELADAAAAAA",
-"Tira, tira, tira ..."
+"Tira, tira, tira ...",
+"Dinheiro não traz felicidade, mas compra cerveja, que é a mesma coisa"
 ];
+
+export let API = {
+  URL:"http://api.cerveja.me/",
+  DEVICE:"device",
+  LOCATION:"location",
+  GOOGLE_GEOCODE:"https://maps.googleapis.com/maps/api/geocode/json?address=#&key=AIzaSyCviMvRgOLra4U-obeRi33K0Cur5WlGTQg",
+  GOOGLE_ADDRESS:"https://maps.googleapis.com/maps/api/geocode/json?latlng=#&key=AIzaSyCviMvRgOLra4U-obeRi33K0Cur5WlGTQg"
+}
+
