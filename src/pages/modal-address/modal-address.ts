a6f2954acf806c2e6fab5bab38c69e68c1cf2339
Jeferson Guardezi <jeferson@guardezi.com>
2017-01-03 23:15:32 -0200

    alguns ajustes

--- src/pages/modal-address/modal-address.ts
+++ src/pages/modal-address/modal-address.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
+import { ModalMapPage } from '../modal-map/modal-map';
 
-import {NavController, Platform, NavParams, ViewController } from 'ionic-angular';
+import {NavController, Platform, NavParams, ViewController,ModalController } from 'ionic-angular';
 
 @Component({
   selector: 'page-modal-address',
@@ -8,15 +9,24 @@ import {NavController, Platform, NavParams, ViewController } from 'ionic-angular
 })
 
 export class ModalAddressPage {
-
+  address;
+  number;
+  complement;
   constructor(
     public platform: Platform,
     public params: NavParams,
     public viewCtrl: ViewController,
-    public navCtrl: NavController
+    public navCtrl: NavController,
+    public modalCtrl : ModalController
+
     ) {  }
 
   dismiss() {
     this.viewCtrl.dismiss();
+  }
+  openModal(){
+    let loca={0:-23,1:-48}
+    let modal = this.modalCtrl.create(ModalMapPage,{"location":loca,"address":this.address+','+this.number+','+this.complement});
+    modal.present();
   }
 }
