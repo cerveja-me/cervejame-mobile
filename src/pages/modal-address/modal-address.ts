0bfab683f49efb3530c7ccd57bf74d976b379d93
Jeferson Guardezi <jeferson@guardezi.com>
2017-01-04 22:50:58 -0200

    ajuste no endereco v2

--- src/pages/modal-address/modal-address.ts
+++ src/pages/modal-address/modal-address.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component,Input,ViewChild } from '@angular/core';
 import { ModalMapPage } from '../modal-map/modal-map';
+import { Device } from '../../providers/device';
 
 import {NavController, Platform, NavParams, ViewController,ModalController } from 'ionic-angular';
 
@@ -12,14 +13,27 @@ export class ModalAddressPage {
   address;
   number;
   complement;
+  addressOptions = [];
+  @ViewChild('input') addressInput ;
+
   constructor(
     public platform: Platform,
     public params: NavParams,
     public viewCtrl: ViewController,
     public navCtrl: NavController,
-    public modalCtrl : ModalController
+    public modalCtrl : ModalController,
+    public _device : Device
 
-    ) {  }
+    ) {
+
+  }
+  ionViewLoaded() {
+
+    setTimeout(() => {
+      this.address.setFocus();
+    },150);
+
+  }
 
   dismiss() {
     this.viewCtrl.dismiss();
@@ -28,5 +42,22 @@ export class ModalAddressPage {
     let loca={0:-23,1:-48}
     let modal = this.modalCtrl.create(ModalMapPage,{"location":loca,"address":this.address+','+this.number+','+this.complement});
     modal.present();
+  }
+
+  setAddress(address){
+    this.address=address['formatted_address'];
+    this.addressOptions=[];
+    // new google.maps.LatLng(address, location[1]);
+    // this.map.setCenter(address['geometry'].location);
+  }
+
+  addressChange(address){
+    if(address.length >3){
+      this._device.getLocationsWithAddres(address)
+      .then((listAddress)=>{
+        console.log('list->',listAddress);
+        this.addressOptions=listAddress['results'];
+      })
+    }
   }
 }
