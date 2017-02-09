ffc3e61205a9b713d8c494c1ffde5f09508fa59d
Jeferson Guardezi <jeferson@guardezi.com>
2017-02-09 01:03:28 -0200

    ajsute

--- doc/sqls.sql
+++ doc/sqls.sql
@@ -7,3 +7,17 @@ and zone is not null
 order by updatedAt desc ;
 
 
+-- ponta grossa ios
+select concat('"',push_token,'",') from device d where (type is null or type ='ios') and d.id
+in (
+  select distinct device
+  from location
+  where zone = '499dfe43-7712-438d-85bf-888b98c7c717');
+
+-- ponta grossa android
+select concat('"',push_token,'",') from device d where ( type ='android') and d.id
+in (
+  select distinct device
+  from location
+  where zone = '499dfe43-7712-438d-85bf-888b98c7c717');
+
--- src/pages/feedback/feedback.ts
+++ src/pages/feedback/feedback.ts
@@ -1,22 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Page1 } from '../page1/page1';
+import { Analytics } from '../../providers/analytics';
 
 /*
   Generated class for the Feedback page.
 
   See http://ionicframework.com/docs/v2/components/#navigation for more info on
   Ionic pages and navigation.
-*/
-@Component({
-  selector: 'page-feedback',
-  templateUrl: 'feedback.html'
-})
-export class FeedbackPage {
+  */
+  @Component({
+    selector: 'page-feedback',
+    templateUrl: 'feedback.html'
+  })
+  export class FeedbackPage {
 
-  constructor(public navCtrl: NavController) {}
+    constructor(public navCtrl: NavController,
+      private an:Analytics) {
+      an.trackView('FeedbackPage','none');
 
-  ionViewDidLoad() {
-    console.log('Hello FeedbackPage Page');
+    }
+    ionViewDidLoad() {
+      console.log('Hello FeedbackPage Page');
+    }
+    gotopage1(){
+      this.navCtrl.push(Page1);
+    }
   }
-
-}
