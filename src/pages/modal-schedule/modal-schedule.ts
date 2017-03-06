aa46e7a7f7eda45527f14f7b139a117edeeda359
Jeferson Guardezi <guardezi@ambev.com.br>
2017-03-06 18:11:37 -0300

    ajuste nos horarios

--- src/pages/modal-schedule/modal-schedule.ts
+++ src/pages/modal-schedule/modal-schedule.ts
@@ -1,22 +1,25 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,NavParams } from 'ionic-angular';
 
 /*
   Generated class for the ModalSchedule page.
 
   See http://ionicframework.com/docs/v2/components/#navigation for more info on
   Ionic pages and navigation.
-*/
-@Component({
-  selector: 'page-modal-schedule',
-  templateUrl: 'modal-schedule.html'
-})
-export class ModalSchedulePage {
+  */
+  @Component({
+      selector: 'page-modal-schedule',
+      templateUrl: 'modal-schedule.html'
+  })
+  export class ModalSchedulePage {
+      hours;
+      day = new Date().getDay();
 
-  constructor(public navCtrl: NavController) {}
+      constructor(public navCtrl: NavController,    public params: NavParams
+          ) {}
 
-  ionViewDidLoad() {
-    console.log('Hello ModalSchedulePage Page');
+      ionViewDidLoad() {
+          this.hours= this.params.get("hours");
+      }
+
   }
-
-}
