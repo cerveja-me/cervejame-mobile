eff2d24fe7d499d15e17aea1abbee95051e547d7
Jeferson Guardezi <jeferson@guardezi.com>
2017-03-07 19:01:58 -0300

    hshshsh

--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -49,17 +49,9 @@ export class Page1 {
     EmailComposer.isAvailable().then((available: boolean) =>{
       if(available) {
         let email = {
-          to: 'max@mustermann.de',
-          cc: 'erika@mustermann.de',
-          bcc: ['john@doe.com', 'jane@doe.com'],
-          attachments: [
-          'file://img/logo.png',
-          'res://icon.png',
-          'base64:icon.png//iVBORw0KGgoAAAANSUhEUg...',
-          'file://README.pdf'
-          ],
-          subject: 'Cordova Icons',
-          body: 'How are you? Nice greetings from Leipzig',
+          to: 'euquero@cerveja.me',
+          subject: 'Na minha Cidade',
+          body: '',
           isHtml: true
         };
 
