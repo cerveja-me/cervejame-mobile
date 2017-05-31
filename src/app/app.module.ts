0d8689440a211cf1eb3a97b3579fc072e221e62c
Jeferson Guardezi <jeferson@guardezi.com>
2017-05-31 00:45:53 -0300

    ajuste no package name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -173,11 +173,11 @@ class GeolocationMock extends Geolocation {
     Geolocation,
     Facebook,
 
-    {provide:Facebook,useClass:FacebookMock}, //coment before build to mobile
-    {provide:Device,useClass:DeviceMock}, //coment before build to mobile
-    {provide:AppVersion,useClass:AppVersionMock},//coment before build to mobile
-    {provide:Push,useClass:PushMock},//coment before build to mobile
-    {provide:Keyboard,useClass:KeyboardMock},//coment before build to mobile
+    // {provide:Facebook,useClass:FacebookMock}, //coment before build to mobile
+    // {provide:Device,useClass:DeviceMock}, //coment before build to mobile
+    // {provide:AppVersion,useClass:AppVersionMock},//coment before build to mobile
+    // {provide:Push,useClass:PushMock},//coment before build to mobile
+    // {provide:Keyboard,useClass:KeyboardMock},//coment before build to mobile
     // {provide:Geolocation,useClass:GeolocationMock},//coment before build to mobile
 
 
