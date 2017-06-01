6bbc559e3199f3f1591f9e793dfa3d57826c26c8
Jeferson Guardezi <jeferson@guardezi.com>
2017-06-01 00:47:40 -0300

    ajuste notification

--- src/pages/notification-modal/notification-modal.ts
+++ src/pages/notification-modal/notification-modal.ts
@@ -1,26 +1,30 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
 
 import { DeviceProvider } from '../../providers/device/device';
 
 @Component({
-    selector: 'page-notification-modal',
-    templateUrl: 'notification-modal.html',
+  selector: 'page-notification-modal',
+  templateUrl: 'notification-modal.html',
 })
 export class NotificationModalPage {
 
-    constructor(
-        public navCtrl: NavController,
-        public navParams: NavParams,
-        public device:DeviceProvider) {
-    }
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public viewCtrl:ViewController,
+    public device:DeviceProvider) {
+  }
 
-    ionViewDidLoad() {
-        console.log('ionViewDidLoad NotificationModalPage');
-    }
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad NotificationModalPage');
+  }
 
-    accept(){
-        this.device.startPush();
-    }
+  accept(){
+    this.device.startPush();
+    setTimeout(() => {
+      this.viewCtrl.dismiss();
+    },300);
+  }
 
 }
