aa71a3d1d7ec49cd920bd415f991b604975ca2e8
Jeferson Guardezi <jeferson@guardezi.com>
2017-06-09 18:45:58 -0300

    ajustes notificacao

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -208,4 +208,3 @@ export class HomePage {
       this.navCtrl.setRoot(HomePage);
     }
   }
-}
--- src/providers/device/device.ts
+++ src/providers/device/device.ts
@@ -43,14 +43,10 @@ declare var UXCam:any;
         this.startOneSignal();
       }
       this.createDevice('empty');
-      if(this.device.platform==='Android' || this.device.platform==='android'){
-        this.startPush();
-      }
-
     }
     createDevice(push:string){
       var d = {
-        other:this.device.manufacturer+' | '+this.device.serial,
+        other:"onesignal",
         model:this.device.model,
         type:this.device.platform,
         id:this.device.uuid,
@@ -72,143 +68,147 @@ declare var UXCam:any;
 
     startPush(){
       this.oneSignal.registerForPushNotifications();
-      const options: PushOptions = {
-        android: {
-          senderID: "10339294539",
-          sound: 'true',
-          icon:'icon'
-        },
-        ios: {
-          senderID: "10339294539",
-          alert: "true",
-          badge: true,
-          sound: 'true'
-        }
-      };
-      this.push.hasPermission()
-      .then((res: any) => {
-        if (res.isEnabled) {
-          console.log('We have permission to send push notifications');
-        } else {
-          console.log('We do not have permission to send push notifications');
-        }
+      this.oneSignal.getIds()
+      .then(res=>{
+        this.createDevice(res.userId);
+        console.log('id->',res.userId,res.pushToken);
+      })
+      // const options: PushOptions = {
+        //   android: {
+          //     senderID: "10339294539",
+          //     sound: 'true',
+          //     icon:'icon'
+          //   },
+          //   ios: {
+            //     senderID: "10339294539",
+            //     alert: "true",
+            //     badge: true,
+            //     sound: 'true'
+            //   }
+            // };
+            // this.push.hasPermission()
+            // .then((res: any) => {
+              //   if (res.isEnabled) {
+                //     console.log('We have permission to send push notifications');
+                //   } else {
+                  //     console.log('We do not have permission to send push notifications');
+                  //   }
 
-      });
-      const pushObject=this.push.init(options);
+                  // });
+                  // const pushObject=this.push.init(options);
 
-      pushObject.on('registration')
-      .subscribe((registration: any) =>{
-        this.createDevice(registration.registrationId);
-      } );
-      pushObject.on('notification')
-      .subscribe((notification: any)=>{
-        if(notification && notification.title && (notification.title=="Pedido Confirmado" || notification.title=="Cerveja a caminho")){
-          this.events.publish('push:order_update', notification);
-        }else if( notification.title=="Cerveja entregue" ){
-          this.events.publish('push:order_complete', notification);
-        }
-        this.doAlert(notification);
+                  // pushObject.on('registration')
+                  // .subscribe((registration: any) =>{
+                    //   this.createDevice(registration.registrationId);
+                    // } );
+                    // pushObject.on('notification')
+                    // .subscribe((notification: any)=>{
+                      //   if(notification && notification.title && (notification.title=="Pedido Confirmado" || notification.title=="Cerveja a caminho")){
+                        //     this.events.publish('push:order_update', notification);
+                        //   }else if( notification.title=="Cerveja entregue" ){
+                          //     this.events.publish('push:order_complete', notification);
+                          //   }
+                          //   this.doAlert(notification);
 
-      });
-      pushObject.on('error')
-      .subscribe((error: any) =>{
-        console.log('error', error);
-      } );
+                          // });
+                          // pushObject.on('error')
+                          // .subscribe((error: any) =>{
+                            //   console.log('error', error);
+                            // } );
 
 
-    }
-    doAlert(data) {
-      let alert = this.alertCtrl.create({
-        title: data.title,
-        message: data.message,
-        buttons: ['Ok']
-      });
-      alert.present()
-    }
-    camPage(page){
-      if(this.platform.is('cordova')){
-        UXCam.tagScreenName(page);
-      }
-    }
-    startOneSignal(){
-      var settings:any={kOSSettingsKeyAutoPrompt:false};
-      this.oneSignal.iOSSettings(settings);
-      // settings:{
-        //    kOSSettingsKeyAutoPrompt:false;kOSSettingsKeyInAppLaunchURL:false}});
-        this.oneSignal.startInit('5d5587e7-348c-4172-8a19-7e01c49daa2a', '10339294539');
-        this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.InAppAlert);
-        this.oneSignal.handleNotificationReceived().subscribe((text) => {
-          console.log('received-> ',text.payload);
-        });
-        this.oneSignal.handleNotificationOpened().subscribe((text) => {
-          console.log('Opened-> ',text.notification.payload);
-        });
-        this.oneSignal.getIds()
-        .then(res=>{
-          console.log('id->',res);
-        })
-        this.oneSignal.endInit();
-      }
-      oneSignalTag(tag:string,zone:string){
-        if(this.platform.is('cordova')){
-          this.oneSignal.sendTag(tag, zone);
-        }
-      }
+                          }
+                          doAlert(data) {
+                            let alert = this.alertCtrl.create({
+                              title: data.title,
+                              message: data.message,
+                              buttons: ['Ok']
+                            });
+                            alert.present()
+                          }
+                          camPage(page){
+                            if(this.platform.is('cordova')){
+                              UXCam.tagScreenName(page);
+                            }
+                          }
+                          startOneSignal(){
+                            var settings:any={kOSSettingsKeyAutoPrompt:false};
+                            this.oneSignal.iOSSettings(settings);
+                            this.oneSignal.startInit('5d5587e7-348c-4172-8a19-7e01c49daa2a', '10339294539');
+                            this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.InAppAlert);
+                            this.oneSignal.handleNotificationReceived().subscribe((text) => {
+                              console.log('received-> ',text.payload);
+                            });
+                            this.oneSignal.handleNotificationOpened().subscribe((text) => {
+                              console.log('Opened-> ',text.notification.payload);
+                            });
+                            this.oneSignal.getIds()
+                            .then(res=>{
+                              this.createDevice(res.userId);
+                              console.log('id->',res.userId,res.pushToken);
+                            })
+                            this.oneSignal.endInit();
+                          }
+                          oneSignalTag(tag:string,zone:string){
+                            if(this.platform.is('cordova')){
+                              this.oneSignal.sendTag(tag, zone);
+                            }
+                          }
 
-      post(url, object){
-        return new Promise((resolve, reject)=> {
+                          post(url, object){
+                            return new Promise((resolve, reject)=> {
 
-          var body = JSON.stringify(object);
-          var headers = new Headers({ 'Content-Type': 'application/json' });
-          var options = new RequestOptions({ headers: headers, method: "post" });
-          this.http.post(url, body,options)
-          .toPromise()
-          .then( (res)=> {
-            resolve(res.json());
-          })
-          .catch((err)=> {
-            reject(err);
-          });
-        });
-      }
-      get(url){
-        return this.http.get(url).toPromise();
-      }
-      put(url,object){
+                              var body = JSON.stringify(object);
+                              var headers = new Headers({ 'Content-Type': 'application/json' });
+                              var options = new RequestOptions({ headers: headers, method: "post" });
+                              this.http.post(url, body,options)
+                              .toPromise()
+                              .then( (res)=> {
+                                resolve(res.json());
+                              })
+                              .catch((err)=> {
+                                reject(err);
+                              });
+                            });
+                          }
+                          get(url){
+                            return this.http.get(url).toPromise();
+                          }
+                          put(url,object){
 
-      }
+                          }
 
-      API:string='http://api.cerveja.me/';
+                          API:string='http://api.cerveja.me/';
 
-      DEVICE:string = 'v2/device/';
-      AUTH:string='auth/login';
-      LOCATION:string = 'location/';
-      COSTUMER:string = 'costumer/';
-      COSTUMER_UPDATE:string = 'update/';
-      LASTBUY:string = 'lastbuy/';
-      LASTBUYOPEN:string = 'lastbuyOpen/';
-      SEND_FEEDBACK:string='sendfeedback/';
-      SALE:string = 'sale/';
-      GOOGLE_GEOCODE:string ='https://maps.googleapis.com/maps/api/geocode/json?address=#&rankby=distance&key=AIzaSyCviMvRgOLra4U-obeRi33K0Cur5WlGTQg';
-      GOOGLE_ADDRESS:string = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=#&key=AIzaSyCviMvRgOLra4U-obeRi33K0Cur5WlGTQg';
-      FIRSTIME:string = 'ftime';
-      PUSH:string='fcm_token';
+                          DEVICE:string = 'v2/device/';
+                          AUTH:string='auth/login';
+                          LOCATION:string = 'location/';
+                          COSTUMER:string = 'costumer/';
+                          COSTUMER_UPDATE:string = 'update/';
+                          LASTBUY:string = 'lastbuy/';
+                          LASTBUYOPEN:string = 'lastbuyOpen/';
+                          SEND_FEEDBACK:string='sendfeedback/';
+                          SALE:string = 'sale/';
+                          GOOGLE_GEOCODE:string ='https://maps.googleapis.com/maps/api/geocode/json?address=#&rankby=distance&key=AIzaSyCviMvRgOLra4U-obeRi33K0Cur5WlGTQg';
+                          GOOGLE_ADDRESS:string = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=#&key=AIzaSyCviMvRgOLra4U-obeRi33K0Cur5WlGTQg';
+                          FIRSTIME:string = 'ftime';
+                          PUSH:string='fcm_token';
 
-      phrases =[
-      "Dinheiro não traz felicidade, mas compra cerveja, que é a mesma coisa.",
-      "Não deixe pra amanhã a cerveja que você pode beber hoje.",
-      "Se dirigir não beba, se beber chame o Cerveja.me!",
-      "Nunca fiz um amigo bebendo leite.",
-      "Diga-me com quem tu andas, que te direi quantas cervejas levar.",
-      "Previsão do tempo: 100% propício para uma cerveja.",
-      "O líquido mais precioso do mundo é a água, pois com ela dá pra fazer cerveja.",
-      "Senhor, dai-me café para mudar o que posso e cerveja para mudar as que não posso.",
-      "Aqui, Cerveja mais gelada que o coração do(a) ex.",
-      "Cerveja é igual banho, tem que tomar todo dia."
-      ];
+                          phrases =[
+                          "Dinheiro não traz felicidade, mas compra cerveja, que é a mesma coisa.",
+                          "Não deixe pra amanhã a cerveja que você pode beber hoje.",
+                          "Se dirigir não beba, se beber chame o Cerveja.me!",
+                          "Nunca fiz um amigo bebendo leite.",
+                          "Diga-me com quem tu andas, que te direi quantas cervejas levar.",
+                          "Previsão do tempo: 100% propício para uma cerveja.",
+                          "O líquido mais precioso do mundo é a água, pois com ela dá pra fazer cerveja.",
+                          "Senhor, dai-me café para mudar o que posso e cerveja para mudar as que não posso.",
+                          "Aqui, Cerveja mais gelada que o coração do(a) ex.",
+                          "Cerveja é igual banho, tem que tomar todo dia."
+                          ];
 
-      getRandonLoading(){
-        return this.phrases[Math.floor(Math.random()*(this.phrases.length-0+1)+0)];
-      }
+                          getRandonLoading(){
+                            return this.phrases[Math.floor(Math.random()*(this.phrases.length-0+1)+0)];
+                          }
+                        }
 
-    }
