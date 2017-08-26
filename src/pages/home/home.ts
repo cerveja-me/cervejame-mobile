b42c74cacdb066b6cf9b16b1f2a3c7d91f6176d4
guardezi <jefersonguardezi@gmail.com>
2017-08-26 02:25:36 -0300

    juasoijdo

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -28,7 +28,7 @@ export class HomePage {
   closed:boolean;
   products:any=[];
   @ViewChild(Slides) slides: Slides;
-
+  amount=2;
   loadedcompleted;
   err:string;
   sale;
@@ -58,6 +58,16 @@ export class HomePage {
 
   }
 
+  increaseAmount(){
+      this.amount++;
+      this.zone.run(()=>{});
+  }
+  decreaseAmount(){
+      if(this.amount>1){
+          this.amount--;
+          this.zone.run(()=>{});
+      }
+  }
   loader=this.load.create({
     content: this.device.getRandonLoading()
   })
@@ -167,6 +177,7 @@ export class HomePage {
       this.device.camPage('home');
     });
   }
+
   scheduleisopen=false;
   openSchedule(){
     if(!this.scheduleisopen){
