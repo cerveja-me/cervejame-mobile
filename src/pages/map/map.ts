114c424c06affa27beb46a39ca8d13d47966f1cf
guardezi <jefersonguardezi@gmail.com>
2017-08-27 17:33:54 -0300

    ajustes no mapa criado o botão centralizar

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -86,6 +86,11 @@ export class MapPage {
 
                 //this.updateAddress({0:this.map.getCenter().lat(),1:this.map.getCenter().lng()});
               });
+              var centerControlDiv = document.createElement('div');
+                var centerControl = new this.centerControl(centerControlDiv, this.map,this.geoLoc);
+
+                // centerControlDiv.index = 1;
+                this.map.controls[google.maps.ControlPosition.BOTTOM_CENTER].push(centerControlDiv);
           })
           .catch(e=>{
               console.log(e);
@@ -93,6 +98,46 @@ export class MapPage {
           });
 
       }
+      centerControl(controlDiv, map,geo) {
+
+        // Set CSS for the control border.
+        var controlUI = document.createElement('div');
+        controlUI.style.backgroundColor = '#fff';
+        controlUI.style.border = '2px solid #fff';
+        controlUI.style.borderRadius = '3px';
+        controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+        controlUI.style.cursor = 'pointer';
+        controlUI.style.marginBottom = '22px';
+        controlUI.style.textAlign = 'center';
+        controlUI.title = 'Click to recenter the map';
+        controlDiv.appendChild(controlUI);
+
+        // Set CSS for the control interior.
+        var controlText = document.createElement('div');
+        controlText.style.color = 'rgb(25,25,25)';
+        controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
+        controlText.style.fontSize = '16px';
+        controlText.style.lineHeight = '38px';
+        controlText.style.paddingLeft = '5px';
+        controlText.style.paddingRight = '5px';
+        controlText.innerHTML = '<span class="icon-location"></span>';
+        controlUI.appendChild(controlText);
+
+        // Setup the click event listeners: simply set the map to Chicago.
+        controlUI.addEventListener('click', function() {
+          geo.getCurrentPosition()
+          .then(l=>{
+            console.log('currente ->',l );
+          })
+          var chicago = {lat: 41.85, lng: -87.65};
+
+          map.setCenter(chicago);
+        });
+
+      }
+
+
+
 
       updateAddress(_loc){
           this.geoLoc.getAddressFromLocation(_loc)
