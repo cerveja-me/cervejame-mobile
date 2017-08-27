892717a33013f24c89d674b36eeffc84c41e361c
Jeferson Guardezi <guardezi@Jefersons-MacBook-Pro.local>
2017-08-27 02:21:53 -0300

    ajustes

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -83,15 +83,13 @@ export class MapPage {
     openAddressEdit(){
       let addressModal = this.modalCtrl.create(MapAddressPage,{"address":this.address,"complement":this.complement});
       addressModal.present();
-    }
+      addressModal.onDidDismiss(data=>{
+          if(data){
+            this.device.camPage("map");
+            console.log('novo endereço -> ',data);
+          }
 
-    addressChange(){
-        if(this.address.formated.length >3){
-            this.geoLoc.getLocationsWithAddres(this.fulladdress)
-            .then((listAddress)=>{
-                this.addressOptions=listAddress['results'];
-            })
-        }
+      })
     }
 
 
