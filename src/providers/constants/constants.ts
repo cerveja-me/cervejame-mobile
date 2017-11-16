c7735d0bae2cc29d7f61d4cd62143ce8e066e12d
guardezi <guardezi@cerveja.me>
2017-11-16 16:23:40 -0200

    ajuste para funcionar

--- src/providers/constants/constants.ts
+++ src/providers/constants/constants.ts
@@ -2,12 +2,12 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ConstantsProvider {
-  public API:string='https://localhost:9001/api/';
+  public API:string='https://192.168.100.124:9001/api/';
   public DEVICE:string='device/';
   public LOCATION:string='location/';
   public PROFILE:string='profile/';
   public AUTH:string=this.PROFILE+'auth/';
-  public REMOTE_ASSETS:string='http://localhost:8080/';
+  public REMOTE_ASSETS:string='http://192.168.100.124:8080/';
   //public REMOTE_ASSETS:string='https://assets.cerveja.me/';
   GOOGLE_GEOCODE:string ='https://maps.googleapis.com/maps/api/geocode/json?address=#&location=LAT,LNG&rankby=distance&key=AIzaSyCviMvRgOLra4U-obeRi33K0Cur5WlGTQg';
   GOOGLE_ADDRESS:string = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=#&key=AIzaSyCviMvRgOLra4U-obeRi33K0Cur5WlGTQg';
