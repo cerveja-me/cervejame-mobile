753509631252c09130be75a5751a05f45d9d45d9
guardezi <guardezi@cerveja.me>
2017-11-25 14:06:45 -0200

    ajustes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -63,58 +63,58 @@ class DeviceMock extends Device{
   get version(): string { return "7.1.1"; }
 }
 
-class FacebookMock extends Facebook{
-  constructor(){super();}
-  login(permissions: string[]) {
-    return new Promise((resolve, reject) => {
-      resolve(
-        {
-          status: "connected",
-          authResponse: {
-            session_key: true,
-            accessToken: "aoijaoisjdo",
-            expiresIn: 5183979,
-            sig: "...",
-            secret: "...",
-            userID: "100110"
-          }
-        }
-      )
-    })
-  }
-
-  api(requestPath: string, permissions: string[]): Promise<any> {
-
-    if (requestPath == '/me?fields=id,name,email,first_name,last_name,gender') {
-      return new Promise((resolve, reject) => {
-        resolve(
-          {
-            id: "191919191",
-            name: "Jeferson Solis de la Torre",
-            first_name: "Jeferson",
-            last_name: "Solis de la Torre",
-            gender: "male",
-            email:"matiassolis@gmail.com"
-          }
-        )
-      })
-
-    }
-
-    return new Promise((resolve, reject) => {
-      resolve(
-        {
-          data: {
-            is_silhouette: false,
-            url: "Thumbnail"
-          }
-        }
-      )
-    })
-
-  }
-
-}
+// class FacebookMock extends Facebook{
+//   constructor(){super();}
+//   login(permissions: string[]) {
+//     return new Promise((resolve, reject) => {
+//       resolve(
+//         {
+//           status: "connected",
+//           authResponse: {
+//             session_key: true,
+//             accessToken: "aoijaoisjdo",
+//             expiresIn: 5183979,
+//             sig: "...",
+//             secret: "...",
+//             userID: "100110"
+//           }
+//         }
+//       )
+//     })
+//   }
+//
+//   api(requestPath: string, permissions: string[]): Promise<any> {
+//
+//     if (requestPath == '/me?fields=id,name,email,first_name,last_name,gender') {
+//       return new Promise((resolve, reject) => {
+//         resolve(
+//           {
+//             id: "191919191",
+//             name: "Jeferson Solis de la Torre",
+//             first_name: "Jeferson",
+//             last_name: "Solis de la Torre",
+//             gender: "male",
+//             email:"matiassolis@gmail.com"
+//           }
+//         )
+//       })
+//
+//     }
+//
+//     return new Promise((resolve, reject) => {
+//       resolve(
+//         {
+//           data: {
+//             is_silhouette: false,
+//             url: "Thumbnail"
+//           }
+//         }
+//       )
+//     })
+//
+//   }
+//
+// }
 
 class SplashScreenMock extends SplashScreen{
   hide(){}
@@ -177,7 +177,7 @@ registerLocaleData(ptBr)
     AppVersion,
     Geolocation,
     OneSignal,
-    { provide: Facebook, useClass: FacebookMock },
+    // { provide: Facebook, useClass: FacebookMock },
     { provide: AppVersion, useClass: AppVersionMock },
     { provide: SplashScreen, useClass:SplashScreenMock },
     { provide: Device, useClass: DeviceMock },
