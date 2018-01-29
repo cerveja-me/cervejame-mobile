e0717590fb6837322e928248cc3458b43980ca42
guardezi <guardezi@cerveja.me>
2018-01-29 14:26:23 -0200

    ajuste e varios

--- src/providers/constants/constants.ts
+++ src/providers/constants/constants.ts
@@ -5,7 +5,7 @@ export class ConstantsProvider {
   //  public API:string='http://ec2-54-207-94-27.sa-east-1.compute.amazonaws.com/api/';
   //  public API:string='http://localhost:1337/ec2-54-207-94-27.sa-east-1.compute.amazonaws.com/api/';
   //  public API:string='http://10.42.0.1:9001/api/';
-  public API: string = 'http://6afb1486.ngrok.io/api/';
+  public API: string = 'http://999eb3d2.ngrok.io/api/';
   public DEVICE: string = 'device/';
   public LOCATION: string = 'location/';
   public PROFILE: string = 'profile/';
@@ -19,5 +19,5 @@ export class ConstantsProvider {
   UXCAM_KEY: string = '170c2a7fdadbaea';//tresux@cerveja.me
   GOOGLE_GEOCODE: string = 'https://maps.googleapis.com/maps/api/geocode/json?address=#&location=LAT,LNG&rankby=distance&key=AIzaSyCviMvRgOLra4U-obeRi33K0Cur5WlGTQg';
   GOOGLE_ADDRESS: string = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=#&key=AIzaSyCviMvRgOLra4U-obeRi33K0Cur5WlGTQg';
-  SHARE_MESSAGE: 'Compartlhando com vc, use meu cupom e ganhe R$ 10';
+  SHARE_MESSAGE:string= 'Compartlhando com vc, use meu cupom e ganhe R$ 10';
 }
